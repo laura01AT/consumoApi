2cc75437f8dc5b096de00e7c445c47f7e4ac45f1
laura01AT <laura.tinoco@agilethought.com>
2023-07-28 16:54:18 -0600

    agregando seccion comentarios en mangaScrapper

--- js/manga.js
+++ js/manga.js
@@ -11,7 +11,6 @@ const options = {
 		'X-RapidAPI-Host': 'manga-scrapper.p.rapidapi.com'
 	}
 };
-
 /*Funcion getData(), consume la api, transforma los datos en json
 leaplica la funcion creteCards()*/
 function getData(){
@@ -45,10 +44,106 @@ function createCards(json){
     </div>
     `
   });
-  console.log(cardContent);
+  //console.log(cardContent);
   cards.innerHTML = cardContent;
   document.getElementById("preloader").style.display = "none";
 }
 
 //Se ejecuta la funcion principal
 getData();
+
+//>>>>>>>>>>>>>>> Script de Comentarios <<<<<<<<<<<<<<<<<<
+const commentForm = document.getElementById('commentForm');
+const commentsContainer = document.getElementById('commentsContainer');
+
+// Mostrar comentarios
+function displayComments() {
+  // Obtener comentarios del Local Storage
+  const comments = JSON.parse(localStorage.getItem('comments')) || [];
+
+  // Limpiar contenedor de comentaris antes de mostrarlos
+  commentsContainer.innerHTML = '';
+
+  // Mostrar cada comentario en contenedor
+  comments.forEach((comment) => {
+    const { name, commentText, timestamp } = comment;
+    const commentDiv = document.createElement('div');
+    commentDiv.classList.add('border', 'p-3', 'mb-3');
+
+    const commentHeader = document.createElement('div');
+    commentHeader.classList.add('font-weight-bold');
+    commentHeader.textContent = `${name} - ${new Date(timestamp).toLocaleString()}`;
+
+    const commentTextElement = document.createElement('div');
+    commentTextElement.textContent = commentText;
+
+    commentDiv.appendChild(commentHeader);
+    commentDiv.appendChild(commentTextElement);
+
+    commentsContainer.appendChild(commentDiv);
+  });
+}
+
+// >>>>>>>>>>>>>>>>>>>> Comentarios <<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// Mostrar comentarios
+function displayComments() {
+  // Obtener comentarios del Local Storage
+  const comments = JSON.parse(localStorage.getItem('comments')) || [];
+
+  // Limpiar contenedor de comentaris antes de mostrarlos
+  commentsContainer.innerHTML = '';
+
+  // Mostrar cada comentario en contenedor
+  comments.forEach((comment) => {
+    const { name, commentText, timestamp } = comment;
+    const commentDiv = document.createElement('div');
+    commentDiv.classList.add('border', 'p-3', 'mb-3');
+
+    const commentHeader = document.createElement('div');
+    commentHeader.classList.add('font-weight-bold');
+    commentHeader.textContent = `${name} - ${new Date(timestamp).toLocaleString()}`;
+
+    const commentTextElement = document.createElement('div');
+    commentTextElement.textContent = commentText;
+
+    commentDiv.appendChild(commentHeader);
+    commentDiv.appendChild(commentTextElement);
+
+    commentsContainer.appendChild(commentDiv);
+  });
+}
+
+// Agregar nuevo comentario
+function addComment(event) {
+  event.preventDefault();
+
+  const nameInput = document.getElementById('name');
+  const commentInput = document.getElementById('comment');
+
+  const name = nameInput.value;
+  const commentText = commentInput.value;
+  const timestamp = Date.now();
+
+  // Obtener comentarios del Local Storage
+  const comments = JSON.parse(localStorage.getItem('comments')) || [];
+
+  // Agregar nuevo comentario a la lista
+  comments.push({ name, commentText, timestamp });
+
+  // Guardar lista actualizada en Local Storage
+  localStorage.setItem('comments', JSON.stringify(comments));
+
+  // Limpiar campos formulario
+  nameInput.value = '';
+  commentInput.value = '';
+
+  // Mostrar comentarios actualizados
+  displayComments();
+}
+
+// Agregar un evento 'submit' al formulario para agregar comentarios
+commentForm.addEventListener('submit', addComment);
+
+// Mostrar comentarios al cargar página
+displayComments();
